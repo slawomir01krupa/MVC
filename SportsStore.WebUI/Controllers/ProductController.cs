657d56591f0d97e3c54cd817cccbfe53a20db5fd
slawomir01krupa <slawomir01krupa@gmail.com>
2014-07-22 14:30:06 +0100

    first

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+      
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult List(int page = 1) 
+        {
+            //this is change 
+            return View(
+                repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));                              
+        }
+       
+
+    }
+}
