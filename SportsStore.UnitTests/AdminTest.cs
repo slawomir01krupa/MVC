5a1d2f32311f6b51a8a95f6421cdb2470fbaecae
slawomir01krupa <slawomir01krupa@gmail.com>
2014-08-08 14:26:44 +0100

    friday commit

---
+++ SportsStore.UnitTests/AdminTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminTest
+    {
+        [TestMethod]
+        public void Index_Contains_All_Products()
+        {
+            //Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                    new Product {ProductID = 1, Name = "P1"},
+                    new Product {ProductID = 2, Name = "P2"},
+                    new Product {ProductID = 3, Name = "P3"},
+                    }.AsQueryable());
+
+
+        }
+
+
+
+    }
+}
